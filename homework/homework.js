91563d6fa88ef85052718332b023fe0c496c970b
k-mm <63869476+k-mm@users.noreply.github.com>
2020-04-26 19:02:35 -0700

    Add files via upload

---
+++ homework/homework.js
@@ -0,0 +1,8 @@
+var name = window.prompt("Let's play the sum game.\n Please enter your name \n ");
+var number = window.prompt("Enter first number");
+var number2 = window.prompt("Enter second number");
+var total = parseInt(number) + parseInt(number2);
+
+
+document.write("Hi," + name +  "!" + " The sum of " + number + " + " + number2 + " is " + total);
+
